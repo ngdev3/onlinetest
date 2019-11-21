e5c685b33de9a824047e7c9541badf3ef5e5dca8
Rajat Gupta <45349777+rajattechnicallibrary@users.noreply.github.com>
2019-11-21 15:37:13 +0530

    Update function_helper.php

--- application/helpers/function_helper.php
+++ application/helpers/function_helper.php
@@ -551,9 +551,9 @@ if (!function_exists('custom_encryption')) {
 
     function custom_encryption($string, $key, $action) {  //echo die($action);
         if ($action == 'encrypt')
-            return base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $string, MCRYPT_MODE_CBC, md5(md5($key))));
+            return ;//base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($key), $string, MCRYPT_MODE_CBC, md5(md5($key))));
         elseif ($action == 'decrypt')
-            return rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($key), base64_decode($string), MCRYPT_MODE_CBC, md5(md5($key))), "\0");
+            return ; //rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($key), base64_decode($string), MCRYPT_MODE_CBC, md5(md5($key))), "\0");
     }
 
 }
