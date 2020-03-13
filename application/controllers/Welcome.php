76c6a2d0405f403c92a1a85a52be5ff71d86c908
Rajat Gupta <rajatinvoice@gmail.com>
2020-03-13 06:12:47 +0000

    Hi

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,5 +21,10 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('welcome_message');
+    }
+    
+    public function privacy()
+	{
+		$this->load->view('welcome_message');
 	}
 }
